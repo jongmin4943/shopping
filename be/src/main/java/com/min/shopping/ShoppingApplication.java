ea16fef76053f9d228cab8c24b8748743e6d929f
Jongmin Yoon <jongmin4943@gmail.com>
2024-10-10 22:07:48 +0900

    init project

---
+++ be/src/main/java/com/min/shopping/ShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.min.shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingApplication.class, args);
+    }
+
+}
---
+++ be/src/test/java/com/min/shopping/ShoppingApplicationTests.java
@@ -0,0 +1,15 @@
+package com.min.shopping;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingApplicationTests {
+
+    @Test
+    @DisplayName("안녕")
+    void contextLoads() {
+    }
+
+}
