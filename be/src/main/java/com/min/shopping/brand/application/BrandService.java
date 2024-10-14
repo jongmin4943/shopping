0d27ce596e72675f8b6cdefa636ac48a4c393990
Jongmin Yoon <jongmin4943@gmail.com>
2024-10-15 00:40:21 +0900

    feat(Brand): 브랜드 등록 기능 추가

--- be/src/main/java/com/min/shopping/brand/application/BrandService.java
+++ be/src/main/java/com/min/shopping/brand/application/BrandService.java
@@ -3,23 +3,32 @@ package com.min.shopping.brand.application;
 import com.min.shopping.brand.application.dto.BrandCreateRequest;
 import com.min.shopping.brand.application.dto.BrandModifyRequest;
 import com.min.shopping.brand.application.dto.BrandResponse;
+import com.min.shopping.brand.domain.Brand;
+import com.min.shopping.brand.domain.BrandRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class BrandService {
 
+    private final BrandRepository brandRepository;
+
     public BrandResponse save(final BrandCreateRequest request) {
-        return null;
+        final Brand brand = new Brand(request.getName());
+
+        final Brand saved = brandRepository.save(brand);
+
+        return BrandResponse.from(saved);
     }
 
     public List<BrandResponse> findAll() {
-        return null;
-    }
-
-    public BrandResponse findById(final Long id) {
-        return null;
+        return brandRepository.findAll()
+                .stream()
+                .map(BrandResponse::from)
+                .toList();
     }
 
     public void update(final Long id, final BrandModifyRequest request) {
--- be/src/main/java/com/min/shopping/brand/application/dto/BrandResponse.java
+++ be/src/main/java/com/min/shopping/brand/application/dto/BrandResponse.java
@@ -1,12 +1,19 @@
 package com.min.shopping.brand.application.dto;
 
+import com.min.shopping.brand.domain.Brand;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class BrandResponse {
     private Long id;
     private String name;
+
+    public static BrandResponse from(final Brand brand) {
+        return new BrandResponse(brand.getId(), brand.getName());
+    }
 }
