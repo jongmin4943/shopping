caf6de296f261d65910b34057c51b45f10582610
Jongmin Yoon <jongmin4943@gmail.com>
2024-10-16 01:59:26 +0900

    feat(Category): 카테고리 갯수 반환 메서드 추가

--- be/src/main/java/com/min/shopping/common/Category.java
+++ be/src/main/java/com/min/shopping/common/Category.java
@@ -2,4 +2,8 @@ package com.min.shopping.common;
 
 public enum Category {
     TOP, OUTER, PANTS, SNEAKERS, BAG, HAT, SOCKS, ACCESSORY;
+
+    public static int countTypes() {
+        return values().length;
+    }
 }
