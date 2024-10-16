47984c1dcb99f6b08710b16d3688a9f25ec3e8da
Jongmin Yoon <jongmin4943@gmail.com>
2024-10-16 21:37:49 +0900

    chore: 더미 데이터용 data.sql 정의

---
+++ be/src/main/resources/data.sql
@@ -0,0 +1,101 @@
+insert into BRAND (NAME, STATUS) values ('A', 'ACTIVE');
+insert into BRAND (NAME, STATUS) values ('B', 'ACTIVE');
+insert into BRAND (NAME, STATUS) values ('C', 'ACTIVE');
+insert into BRAND (NAME, STATUS) values ('D', 'ACTIVE');
+insert into BRAND (NAME, STATUS) values ('E', 'ACTIVE');
+insert into BRAND (NAME, STATUS) values ('F', 'ACTIVE');
+insert into BRAND (NAME, STATUS) values ('G', 'ACTIVE');
+insert into BRAND (NAME, STATUS) values ('H', 'ACTIVE');
+insert into BRAND (NAME, STATUS) values ('I', 'ACTIVE');
+insert into BRAND (NAME, STATUS) values ('J', 'ACTIVE');
+insert into BRAND (NAME, STATUS) values ('K', 'ACTIVE');
+
+-- A 브랜드
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (11200.00, 1, 'TOP');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (5500.00, 1, 'OUTER');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (4200.00, 1, 'PANTS');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (9000.00, 1, 'SNEAKERS');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (2000.00, 1, 'BAG');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (1700.00, 1, 'HAT');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (1800.00, 1, 'SOCKS');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (2300.00, 1, 'ACCESSORY');
+
+-- B 브랜드
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (10500.00, 2, 'TOP');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (5900.00, 2, 'OUTER');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (3800.00, 2, 'PANTS');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (9100.00, 2, 'SNEAKERS');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (2100.00, 2, 'BAG');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (2000.00, 2, 'HAT');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (2000.00, 2, 'SOCKS');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (2200.00, 2, 'ACCESSORY');
+
+-- C 브랜드
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (10000.00, 3, 'TOP');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (6200.00, 3, 'OUTER');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (3300.00, 3, 'PANTS');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (9200.00, 3, 'SNEAKERS');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (2200.00, 3, 'BAG');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (1900.00, 3, 'HAT');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (2200.00, 3, 'SOCKS');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (2100.00, 3, 'ACCESSORY');
+
+-- D 브랜드
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (10100.00, 4, 'TOP');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (5100.00, 4, 'OUTER');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (3000.00, 4, 'PANTS');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (9500.00, 4, 'SNEAKERS');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (2500.00, 4, 'BAG');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (1500.00, 4, 'HAT');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (2400.00, 4, 'SOCKS');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (2000.00, 4, 'ACCESSORY');
+
+-- E 브랜드
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (10700.00, 5, 'TOP');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (5000.00, 5, 'OUTER');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (3800.00, 5, 'PANTS');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (9900.00, 5, 'SNEAKERS');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (2300.00, 5, 'BAG');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (1800.00, 5, 'HAT');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (2100.00, 5, 'SOCKS');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (2100.00, 5, 'ACCESSORY');
+
+-- F 브랜드
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (11200.00, 6, 'TOP');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (7200.00, 6, 'OUTER');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (4000.00, 6, 'PANTS');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (9300.00, 6, 'SNEAKERS');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (2100.00, 6, 'BAG');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (1600.00, 6, 'HAT');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (2300.00, 6, 'SOCKS');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (1900.00, 6, 'ACCESSORY');
+
+-- G 브랜드
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (10500.00, 7, 'TOP');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (5800.00, 7, 'OUTER');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (3900.00, 7, 'PANTS');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (9000.00, 7, 'SNEAKERS');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (2200.00, 7, 'BAG');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (1700.00, 7, 'HAT');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (2100.00, 7, 'SOCKS');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (2000.00, 7, 'ACCESSORY');
+
+-- H 브랜드
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (10800.00, 8, 'TOP');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (6300.00, 8, 'OUTER');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (3100.00, 8, 'PANTS');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (9700.00, 8, 'SNEAKERS');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (2100.00, 8, 'BAG');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (1600.00, 8, 'HAT');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (2000.00, 8, 'SOCKS');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (2000.00, 8, 'ACCESSORY');
+
+-- I 브랜드
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (11400.00, 9, 'TOP');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (6700.00, 9, 'OUTER');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (3200.00, 9, 'PANTS');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (9500.00, 9, 'SNEAKERS');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (2400.00, 9, 'BAG');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (1700.00, 9, 'HAT');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (1700.00, 9, 'SOCKS');
+insert into PRODUCT (PRICE, BRAND_ID, CATEGORY) values (2400.00, 9, 'ACCESSORY');
